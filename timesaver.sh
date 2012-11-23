3fa15c41907906ec8601e60598f6c092967e950b
Andrew Makarov <andrew.r3nya@gmail.com>
2012-11-23 22:12:27 +0400

    Added timesaver script

---
+++ timesaver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "127.0.0.1 www.facebook.com" >> /etc/hosts
+echo "127.0.0.1 www.twitter.com" >> /etc/hosts
+echo "127.0.0.1 www.youtube.com" >> /etc/hosts
+echo "127.0.0.1 vk.com" >> /etc/hosts
+echo "127.0.0.1 www.reddit.com" >> /etc/hosts
+echo "127.0.0.1 www.slashdot.org" >> /etc/hosts
+echo "127.0.0.1 news.ycombinator.com" >> /etc/hosts
+echo "127.0.0.1 www.9gag.com" >> /etc/hosts
