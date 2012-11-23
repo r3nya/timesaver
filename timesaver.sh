6b0c22705b2aa51f7c9887dec0a7c46bb881048a
Andrew Makarov <andrew.r3nya@gmail.com>
2012-11-23 22:14:19 +0400

    Updated timesaver script

--- timesaver.sh
+++ timesaver.sh
@@ -5,5 +5,5 @@ echo "127.0.0.1 www.youtube.com" >> /etc/hosts
 echo "127.0.0.1 vk.com" >> /etc/hosts
 echo "127.0.0.1 www.reddit.com" >> /etc/hosts
 echo "127.0.0.1 www.slashdot.org" >> /etc/hosts
-echo "127.0.0.1 news.ycombinator.com" >> /etc/hosts
+echo "127.0.0.1 habrahabr.ru" >> /etc/hosts
 echo "127.0.0.1 www.9gag.com" >> /etc/hosts
