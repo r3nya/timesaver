fc183190d36b74a41d336d6ae53c2d70d6d15606
Andrew Makarov <andrew.r3nya@gmail.com>
2012-11-23 22:35:27 +0400

    Added timesaver ruby-script

---
+++ timesaver.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+sites = %w( twitter.com habrahabr.ru vk.com facebook.com youtube.com reddit.com tracksflow.com )
+
+File.open("/etc/hosts", "a") { |f|
+	sites.each { |url| 
+		f.puts "127.0.0.1 #{url}"
+	}
+
+}
